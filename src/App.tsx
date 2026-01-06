0920dc7b15bfe1bd468c122d2550bc406c043a44
Goodmoneyinc <goodmoneyinc@proton.me>
2026-01-06 15:41:00 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,15 @@
 import React from 'react';
-import Header from './components/Header';
-import { Header } from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header } from './components/layout/Header';
 import { Home } from './pages/Home';
 import { Pricing } from './pages/Pricing';
 import { Success } from './pages/Success';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { Dashboard } from './pages/Dashboard';
-import Hero from './components/Hero';
-import OnlineCourses from './components/OnlineCourses';
-import LiveVideos from './components/LiveVideos';
-import SubscriptionPlans from './components/SubscriptionPlans';
-import PhotoCollage from './components/PhotoCollage';
-import Footer from './components/Footer';
-import LMSNavigation from './components/LMS/LMSNavigation';
-import CourseOutline from './components/CourseOutline';
-import { SuccessPage } from './pages/SuccessPage';
 
 function App() {
-  const currentPath = window.location.pathname;
-  const showLMS = window.location.hash === '#lms';
-  const showCourseOutline = window.location.hash === '#course-outline';
-
-  if (currentPath === '/success') {
-    return <SuccessPage />;
-  }
-
-  if (showLMS) {
-    return <LMSNavigation />;
-  }
-
-  if (showCourseOutline) {
-    return <CourseOutline />;
-  }
-
   return (
-    <div className="min-h-screen bg-white">
-      <Header />
-      <Hero />
-      <OnlineCourses />
-      <LiveVideos />
-      <SubscriptionPlans />
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Header />
@@ -58,5 +27,3 @@ function App() {
 }
 
 export default App;
-  )
-}
